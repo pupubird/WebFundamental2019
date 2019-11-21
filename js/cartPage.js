819762097e8bd57f537c203fdac4bce4b7648df5
pupubird <rainchai4240@gmail.com>
2019-11-21 13:49:31 +0800

    cart page done

--- js/cartPage.js
+++ js/cartPage.js
@@ -26,7 +26,80 @@ export function cartPage() {
     mobileNavBarTabLinks[2].classList.add('underline');
 
     navbar.classList.add('navbar-shadow');
-    state.innerHTML = '';
+    state.innerHTML = `
+    <div class="card table-wrapper">
+        <table class="cart-table ">
+            <tr class="table-header card">
+                <th width='25%' class="sub-header-font light-primary-color s-font-size">Product</th>
+                <th width='25%' class="sub-header-font light-primary-color s-font-size">Price</th>
+                <th width='25%' class="sub-header-font light-primary-color s-font-size">Quantity</th>
+                <th width='25%' class="sub-header-font primary-color s-font-size">Total</th>
+            </tr>
+            <tr class="table-row">
+                <td class="table-product">
+                    <p class="sub-header-font s-font-size">ecstasy</p>
+                    <img src="assets/stock3.jpg">
+                </td>
+                <td class="table-price">
+                    <p class="primary-color s-font-size action-call-font" >RM100</p>
+                </td>
+                <td class="table-quantity">
+                <div> 
+                    <p class="m-font-size sub-header-font darken-secondary-color">-</p>
+                    <p class="s-font-size sub-header-font primary-color" contenteditable>1</p>
+                    <p class="m-font-size sub-header-font tertiary-color">+</p>
+                </div>
+                </td>
+                <td class="table-total tertiary-color action-call-font s-font-size">RM100</td>
+            </tr>
+            <tr class="table-row">
+                <td class="table-product">
+                    <p class="sub-header-font s-font-size">ecstasy</p>
+                    <img src="assets/stock3.jpg">
+                </td>
+                <td class="table-price">
+                    <p class="primary-color s-font-size action-call-font" >RM100</p>
+                </td>
+                <td class="table-quantity">
+                <div> 
+                    <p class="m-font-size sub-header-font darken-secondary-color">-</p>
+                    <p class="s-font-size sub-header-font primary-color" contenteditable>1</p>
+                    <p class="m-font-size sub-header-font tertiary-color">+</p>
+                </div>
+                </td>
+                <td class="table-total tertiary-color action-call-font s-font-size">RM100</td>
+            </tr>
+            <tr class="table-row">
+                <td class="table-product">
+                    <p class="sub-header-font s-font-size">ecstasy</p>
+                    <img src="assets/stock3.jpg">
+                </td>
+                <td class="table-price">
+                    <p class="primary-color s-font-size action-call-font" >RM100</p>
+                </td>
+                <td class="table-quantity">
+                <div> 
+                    <p class="m-font-size sub-header-font darken-secondary-color">-</p>
+                    <p class="s-font-size sub-header-font primary-color" contenteditable>1</p>
+                    <p class="m-font-size sub-header-font tertiary-color">+</p>
+                </div>
+                </td>
+                <td class="table-total tertiary-color action-call-font s-font-size">RM100</td>
+            </tr>
+            
+        </table>
+        <div class="checkout">
+            <div>
+                <p class="action-call-font primary-color s-font-size">Total: RM100</p>
+                <p class="paragraph-font xs-font-size">Discounts: RM0</p>
+                <button class="medium-button">Update</button>
+                <button class="medium-button highlight-button">Checkout</button>
+            </div>
+        </div
+    </div>
+    
+    
+    `;
     navbar.style.display = "flex";
     document.querySelector('.navbar-padding').style.minHeight = navbar.offsetHeight + 'px';
 }
